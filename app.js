035aa154af751e19308c7b09b0b94f13e0da1396
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-07-31 02:12:29 +0530

    store and retrieve

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Starting Password manager");
+
+var storage = require('node-persist');
+storage.initSync();
+
+// storage.setItemSync('name','Prahalad');
+var name = storage.getItemSync('name');
+console.log('Saved Name is '+name);
